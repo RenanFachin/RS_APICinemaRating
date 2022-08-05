64f2cb5ed561dd47eeb2bbde953f40e72dc13c7b
Renan <renan.fachin@hotmail.com>
2022-08-05 17:00:12 -0300

    Criação do controller para cadastrar uma movie_notes (notes)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,14 @@ const { Router } = require("express");
 
 // importando a função POST do usersRouter
 const usersRouter = require("./users.routes");
+const notesRouter = require("./notes.routes");
 
 // Inicializando a função ROUTER que veio do express e armazenando na constante routes
 const routes = Router();
 
 // Quando for requisitado algo na rota /users, será chamada a função usersRouter que está sendo importada da users.routes.js
 routes.use("/users", usersRouter);
-
+routes.use("/notes", notesRouter);
 
 // Exportando a routes
 module.exports = routes;
