8e699ece64660b8af170654b1668c0cbcb5184bf
Renan <renan.fachin@hotmail.com>
2022-08-05 14:58:31 -0300

    Instalada biblioteca (express-async-errors) para o tratamento de erros

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,27 @@
+// Importando o AppError
+const AppError = require("../utils/AppError");
+
 // Camada que vai executar o que o usuário solicitou pela requisição
 
 class UsersController {
     // Classe permite ter várias funções
 
-    // Função para criar um usuário
+    // Função "CREATE" para criar um usuário
     create (request, response) {
         // obtendo as informações enviadas pela requisição
         const { name, email, password } = request.body;
+        
+        // Verificando se o usuário não informou o name
+        if(!name){
+            throw new AppError("O nome é obrigatório");
+            // new para instanciar. message = "o nome é obrigatório"
+        }
 
         // devolvendo em forma de json as informações fornecidas pelo body da requisição
-        response.json({ name, email, password });
+        response.status(201).json({ name, email, password });
     }
+
+
 }
 
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
+// Importando a biblioteca do express async errors
+require("express-async-errors");
+// Importando o APP ERROR
+const AppError = require("./utils/AppError")
+
 // Importando o express e armazenando na constante express
 const express = require("express");
 // Importando do index.js de routes a função routes que é executada quando algo é enviado pelo /users
@@ -7,6 +12,24 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use( ( error, request, response, next ) => {
+    // sabendo de onde vem o erro, se for do lado do cliente
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+    
+    console.error(error);
+
+    // caso seja um erro do lado do servidor
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    });
+})
 
 const PORT = 3333;
 
